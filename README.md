c6dbf7e50f5fd47337637fe397392a38df4592cd
Shoma Mandal <jinny.sm@gmail.com>
2019-07-08 09:37:20 +0800

    Added src files for softgames tasks

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Game
+# Pixi.JS Tasks
+I made 2 tasks out of 3.
+
+## Tasks Details
+The test is split in 3 parts on landing page. Performance will be checked on mobile (low/mid/high-end devices) plus desktop and should always be fullscreen.
+
+1. Create 144 sprites (NOT graphics object) that are stack on each other like cards (so object above covers bottom one, but not completely). Every second 1 object from top of stack goes to other stack - animation of moving should be 2 seconds long. So at the end of whole process you should have reversed stack. 
+
+2. Create a tool that will allow mixed text and images in an easy way (for example displaying text with emoticons or prices with money icon). It should come up every 2 seconds a random text with images in random configuration (image + text + image, image + image + image, image + image + text, text + image + text etc) and a random font size.
+
+3. Particles - make a demo that shows an awesome fire effect. Please keep number of images low (max 10 sprites on screen at once). Feel free to use existing libraries how you would use them in a real project. WIP
+
+## Used Technologies
+- PixiJS 5.0.4
+- NPM
+- ES6 and Babel Compiler
+- Webpack
+- Webpack Dev Server
---
+++ app.ts
@@ -0,0 +1,199 @@
+import * as PIXI from 'pixi.js';
+
+class Game {
+  app: PIXI.Application;
+  loader = new  PIXI.Loader;
+  sprite =[];
+  spritePosition = [];
+  reverseSpritePosition = [];
+  width = window.innerWidth - 15;
+  height = window.innerHeight - 15;
+  option = {
+    width: this.width,
+    height: this.height,
+    backgroundColor: 0x1099bb
+  }
+  text;
+  totalCards = 53;
+  cardIndex:number = this.totalCards - 1;
+  ticker;
+  speed = 2;
+  newXPosition = this.width/2;
+  scale = 0.5;
+  cardMenu;
+  randomMoney
+  constructor() {
+    new PIXI.Rectangle(0, 0, this.width, this.height);
+    this.app = new PIXI.Application(this.option);
+    document.body.appendChild(this.app.view);
+    this.loader.add('menu1','./assets/card-icon.png');
+    this.loader.add('menu2','./assets/card-icon.png');
+    this.loader.add('cards','./assets/spritesheetcollection.json');
+    this.loader.add('coin','./assets/coin.json');
+    this.loader.load(this.setup.bind(this));
+  }
+
+  setup() {
+    this.cardMenu = new PIXI.Sprite(this.loader.resources["menu1"].texture);
+    this.cardMenu.scale = new PIXI.Point(this.scale, this.scale);
+    this.cardMenu.position.set(this.width/2, this.height/2);
+    this.cardMenu.buttonMode = true;
+    this.cardMenu.interactive = true;
+    this.cardMenu.buttonMode = true;
+    this.cardMenu.on('pointerdown', ()=>{
+        this.onButtonDown(0);
+    });
+    this.app.stage.addChild(this.cardMenu);
+
+    this.randomMoney = new PIXI.Sprite(this.loader.resources["coin"].textures['coin'+0+'.png']);
+    this.randomMoney.scale = new PIXI.Point(this.scale, this.scale);
+    this.randomMoney.position.set(this.width/2, this.height/3);
+    this.randomMoney.buttonMode = true;
+    this.randomMoney.interactive = true;
+    this.randomMoney.buttonMode = true;
+    this.randomMoney.on('pointerdown', ()=>{
+        this.onButtonDown(1);
+    });
+    this.app.stage.addChild(this.randomMoney);
+  }
+  onButtonDown(key){
+    switch (key) {
+        case 0:
+            this.createCard();
+            this.moveCardRight(this.sprite[this.cardIndex],this.cardIndex)
+            break;
+        case 1:
+            this.createRandomMoney();
+            // this.moveCardRight(this.sprite[this.cardIndex],this.cardIndex)
+            break;
+        default:
+            break;
+    }
+  }
+  showAnimationDuration(){
+    this.text = new PIXI.Text('This is a PixiJS text' + this.app.ticker.speed,{fontFamily : 'Arial', fontSize: 23, fill : 0xff1010, align : 'center'});
+    this.app.stage.addChild(this.text);
+  }
+  createRandomMoney(){
+    let position = [this.width/2.5, this.width/2, this.width/1.7];
+    let yPosition = this.height/2;
+    let order = [
+                    [0,0,0],
+                    [1,0,0],
+                    [1,1,0],
+                    [1,1,1],
+                    [0,0,1],
+                    [0,1,1],
+                    [1,0,1]
+                ]
+    // this.ticker = new PIXI.Ticker();
+    // this.ticker.add(() => {
+        setInterval(()=>{
+            for (let index = 0; index < order.length; index++) {
+                let random = order[Math.floor(Math.random() * order.length)];
+                for (let index_ = 0; index_ < order[index].length; index_++) {
+                    for (let ind = 0; ind < position.length; ind++) {
+                        console.log();
+                        
+                        // if(order[index][index_] == 0){
+                        //     this.generateText(position[ind],yPosition);
+                        // }else{
+                        //     this.generateCoin(position[ind],yPosition);
+                        // }
+                    }
+                }
+            }
+            // let random = position[Math.floor(Math.random() * position.length)];
+            
+            // for (let index = 0; index < position.length; index++) {
+            // if(random != position[index]){
+               
+            // }
+            // }
+           
+        },2000)
+        
+    // });
+  }
+  generateText(xPosition,yPosition){
+    let text = new PIXI.Text('PIXI',{fontFamily : 'Arial', fontSize: 23, fill : 0xff1010, align : 'center'});
+    text.position.x = xPosition;
+    text.position.y = yPosition;
+    this.app.stage.addChild(text);
+    setTimeout(() => {
+        text.destroy();
+     }, 1900);
+  }
+  generateCoin(xPosition,yPosition){
+    let textureArray = [];
+    for (let index = 0; index < 6; index++) {
+        const texture = PIXI.Texture.from('coin'+index+'.png');
+        textureArray.push(texture);
+    }
+    let animate = new PIXI.AnimatedSprite(textureArray);
+    animate.position.x = xPosition;
+    animate.position.y = yPosition;
+    animate.animationSpeed = 0.3;
+    animate.play();
+    this.app.stage.addChild(animate);
+    setTimeout(() => {
+       animate.destroy();
+    },1900);
+  }
+  createCard(){
+    let index, x, y;
+    for (index = 0; index < this.totalCards; index++) {
+        let sprite = new PIXI.Sprite(this.loader.resources["cards"].textures['card' + index + '.png']);
+        sprite.scale = new PIXI.Point(this.scale, this.scale);
+        x = index*4;
+        y = index*8;
+        sprite.position.set(x, y);
+        this.app.stage.addChild(sprite);
+        this.sprite.push(sprite);
+        this.spritePosition.push({x: (x + this.newXPosition), y: y, z: index})
+    }
+    for (let index = this.spritePosition.length - 1; index >= 0; index--) {
+       this.reverseSpritePosition.push(this.spritePosition[index]);
+    }
+  }
+  moveCardRight(sprite, index){
+    this.ticker = new PIXI.Ticker();
+    this.ticker.start();
+    this.ticker.speed = this.speed;
+    this.ticker.add(() => {
+        this.updateOrder(index,this.reverseSpritePosition[index].z,sprite);
+        if(sprite.x >= this.reverseSpritePosition[index].x){
+            sprite.x = this.reverseSpritePosition[index].x;
+            this.ticker.destroy();
+            this.moveCardTop(sprite,index);
+        }
+        sprite.x++;
+    });
+  }
+  moveCardTop(sprite,index){
+    this.ticker = new PIXI.Ticker();
+    this.ticker.start();
+    this.ticker.speed = this.setup;   
+    this.ticker.add(() => {
+        if(sprite.y ==  this.reverseSpritePosition[index].y){
+            sprite.y = this.reverseSpritePosition[index].y;
+            this.ticker.destroy();
+            this.cardIndex--;
+            if(this.cardIndex >= 0){
+                this.moveCardRight(this.sprite[this.cardIndex],this.cardIndex);
+            }
+        }
+        if(sprite.y < this.reverseSpritePosition[index].y){
+            sprite.y++;
+        }else{
+            sprite.y--;
+        }
+    });
+  }
+  updateOrder(index,y,sprite){
+    this.app.stage.children[index].zIndex = y;
+    this.app.stage.addChild(sprite);
+  }
+}
+
+new Game();
